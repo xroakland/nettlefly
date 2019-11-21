3615f80e79b05852d7d14c7489374fd45700ff3a
Extinction Rebellion Oakland <57816465+xroakland@users.noreply.github.com>
2019-11-21 00:20:21 -0800

    edit2

---
+++ config.rb
@@ -0,0 +1,23 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+http_path = "C:/Users/intel19/Documents/GitHub/xroakland/nettlefly/html"
+css_dir = ""
+sass_dir = ""
+images_dir = "images"
+generated_images_path = "C:/Users/intel19/Documents/GitHub/xroakland/nettlefly/generatedimages"
+javascripts_dir = "javascripts"
+fonts_dir = "fonts"
+
+output_style = :nested
+
+relative_assets = true
+
+line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass  scss && rm -rf sass && mv scss sass
